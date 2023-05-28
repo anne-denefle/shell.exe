c2bedd24304be8a6ae300bd0213554f18cad539f
anne-denefle <anne.denefle@laplateforme.io>
2023-05-28 19:12:40 +0200

    modifié :         Shell_Userlist.csv 	modifié :         accessrights.sh

--- Job9/accessrights.sh
+++ Job9/accessrights.sh
@@ -3,12 +3,25 @@
 # 
 
 #stocker le parametre dans une variable
-	fichier="/home/anne/Shell.exe/Job9/Shell_Userlist.csv" 
+fichier="/home/anne/Shell.exe/Job9/Shell_Userlist.csv" 
+sed -i 's/ //g' Shell_Userlist.csv
 
 while IFS=',' read Id Prenom Nom Pwd Role
- 
-do
-echo "USER : $Id : $Pwd  $Role  $Prenom  $Nom"
+	do
+	 create_user() { 
+		local username=$1 local password=$2 local id=$3 local role=$4 
+		sudo useradd -m $username -p $password -u $id -G $role
+        echo "ROLE:" $role
+	 if [ "$role" == "Admin" ]; then
+           sudo usermod -aG sudo $username
+         fi
+	echo "l'utilisateur a été créé avec success : $username $id"
+       
 
+
+}
+
+create_user  "$Prenom.$Nom" $Pwd $Id $Role
 done  <$fichier
+
 
